f0128a698518f9775c797e9eeb91ab6fd30ad596
gramaovi <81247073+gramaovi@users.noreply.github.com>
2021-08-17 02:05:24 +0300

    Footer-Done Without responsiveness

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div class="footer-class">
+    <div class="d-flex-top ">
+        <div class="p-2">
+            <a href="#" >Romania</a>
+ 
+        </div>
+
+    </div>
+    <div class="flex">
+        <div class="p-2-left">
+            <a href="#" >About</a>
+            <a href="#" >Advertising</a>
+            <a href="#" >Business</a>
+            <a href="#" >How search works</a>
+        </div>
+        <div class="p-2-right">
+            <a href="#" >Privacy</a>
+            <a href="#" >Terms</a>
+            <a href="#" >Settings</a>
+        </div>
+    </div>
+   </div>
+  
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+@import './styles/footer.css';
+
+
+</style>
