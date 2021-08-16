9ee38b10addae271a01c8d0a4b297a86996d2493
gramaovi <81247073+gramaovi@users.noreply.github.com>
2021-08-16 18:57:05 +0300

    Header(containers->items) NOT aligned 1-1 yet

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <Header></Header>
+  <Google_content></Google_content>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Google_content from './components/Content.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Google_content,
+ 
   }
 }
 </script>
 
-<style>
+<style >
+ body {
+    background-image:url("https://www.google.com/search?q=image&sxsrf=ALeKk00LCGlnxt4ejcIIBCYMpz9aRl1-Dw:1629127911335&tbm=isch&source=iu&ictx=1&fir=gxFxsvFBmxeZ9M%252C0JWe7yDOKrVFAM%252C_&vet=1&usg=AI4_-kReDzFRmzDAOj_DsRSOvMu2NwesqQ&sa=X&ved=2ahUKEwiE8sqz7rXyAhVfhf0HHfBjDEMQ9QF6BAgMEAE&biw=1920&bih=969#imgrc=gxFxsvFBmxeZ9M")!important;
+  
+  }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+   <div class="items">
+    <div class="hrefs">
+      <a href="#">Gmail</a>
+      <a href="#">Images</a>
+    </div>
+    <a href="#">
+      <img class="menu-photo" src="../assets/Google-Apps.png">
+    </a>
+   </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+@import './styles/header.css';
+
+
+</style>
